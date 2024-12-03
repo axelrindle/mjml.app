d5dbefe5be0649df4fe37875a6bb3d95feadbd5b
Axel Rindle <mail@axelrindle.de>
2024-12-03 13:40:14 +0100

    fix: specify router base path

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, basepath: import.meta.env.BASE_URL })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
