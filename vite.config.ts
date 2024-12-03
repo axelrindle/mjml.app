3cc987a62a1db084199d2f801f982944e116e2e6
Axel Rindle <mail@axelrindle.de>
2024-12-03 08:33:20 +0100

    fix: add route tree

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,10 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        router(),
+        router({
+            semicolons: false,
+            quoteStyle: 'single',
+        }),
         react(),
     ],
     resolve: {
